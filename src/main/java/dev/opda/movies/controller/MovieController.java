cd3901459df972007fc9a0e737f563d4e1987fc6
Kiwi_Lin <opda0887@gmail.com>
2024-09-16 16:00:51 +0800

    add CrossOrigin in two controller classes

--- src/main/java/dev/opda/movies/controller/MovieController.java
+++ src/main/java/dev/opda/movies/controller/MovieController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import dev.opda.movies.Movie;
 import dev.opda.movies.service.MovieService;
 
+@CrossOrigin(origins = "*") // allow all origins to access the API
 @RestController
 @RequestMapping("/api/v1/movies") // instead of "localhost:8080/root" -> "localhost:8080/api/v1/movies"
 public class MovieController {
--- src/main/java/dev/opda/movies/controller/ReviewController.java
+++ src/main/java/dev/opda/movies/controller/ReviewController.java
@@ -5,6 +5,7 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import dev.opda.movies.Review;
 import dev.opda.movies.service.ReviewService;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/v1/reviews")
 public class ReviewController {
